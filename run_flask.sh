485e51ba7ea126dc6a4647a764dcb52763c91c7b
D. J. Mack <dj_mack@gmx.de>
2022-10-18 19:51:58 +0200

    refactor: Remove flash shell script - unused

--- run_flask.sh
+++
@@ -1,4 +0,0 @@
-# This will start the REST endpoint at 127.0.0.0:5000/optimize_xp
- export FLASK_APP=xpOptimizerService.py
- export FLASK_ENV=development  # Remove before deployment (see https://flask.palletsprojects.com/en/1.1.x/deploying/#deployment for options)
- flask run
